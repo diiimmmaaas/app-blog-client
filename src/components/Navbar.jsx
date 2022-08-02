a8b136195a7d6e6dbbc872ab3bf98927d9eec81d
diiimmmaaas <dm.sokolov1994@gmail.com>
2022-08-02 15:56:46 +0300

    add authSlice, registerForm, loginForm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,58 @@
 import React from "react";
+import {Link, NavLink} from "react-router-dom";
 
 export const Navbar = () => {
+
+    const isAuth = false
+
+    const activeStyles = {
+        color: "white"
+    }
+
     return (
         <div className={"flex py-4 justify-between items-center"}>
-            <span className={"flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm"}>E</span>
+            <span
+                className={"flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm"}>E</span>
 
-            <ul className="flex gap-8">
-                <li><a href="/" className={"text-xs text-gray-600 hover:text-white"}></a>Главная</li>
-                <li><a href="/" className={"text-xs text-gray-400 hover:text-white"}></a>Мои посты</li>
-                <li><a href="/" className={"text-xs text-gray-400 hover:text-white"}></a>Добавить пост</li>
+            {
+                isAuth && <ul className="flex gap-8">
+                <li>
+                    <NavLink href="/"
+                             to={"/"}
+                             className={"text-xs text-gray-400 hover:text-white"}
+                             style={ ({isActive}) => isActive ? activeStyles : undefined}
+                    >
+                        Главная
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink href="/"
+                             to={"/posts"}
+                             className={"text-xs text-gray-400 hover:text-white"}
+                             style={ ({isActive}) => isActive ? activeStyles : undefined}
+                    >
+                        Мои посты
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink href="/"
+                             to={"/new"}
+                             className={"text-xs text-gray-400 hover:text-white"}
+                             style={ ({isActive}) => isActive ? activeStyles : undefined}
+                    >
+                        Добавить пост
+                    </NavLink>
+                </li>
             </ul>
+            }
+
+
 
             <div className={"flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2"}>
-                <button>Войти</button>
+                {
+                    isAuth ? (<button>Выйти</button>) : <Link to={"/login"}>Войти</Link>
+                }
+
             </div>
         </div>
     );
